38b224310bbd6cf16a762f520653351bb5ef5f02
Siddhant <santoshshrivastava39@gmail.com>
2022-03-10 00:07:39 +0530

    Max 100 hours added

---
+++ Totalworkinghrs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace EmployeeWageProblem
+{
+    public class Totalworkinghrs
+    {
+        public void Calworkinghrs()
+        {
+            int Emp_Rate_Per_Hour = 20;
+            int Emphrs = 0;
+            int Empwage = 0;
+            int total_Emp_hrs = 0;
+            int total_working_days = 0;
+
+            int Total_working_hrs = 100;
+            int Total_working_days = 20;
+            int Totalwage = 0;
+            while (total_Emp_hrs < Total_working_hrs && total_working_days < Total_working_days)
+            {
+                total_working_days++;
+                Random random = new Random();
+                int employeecheck = random.Next(2);
+
+                if (employeecheck == 1)
+                {
+                    Console.WriteLine("Employee is Present");
+                    Emphrs = 8;
+                }
+                else
+                {
+                    Console.WriteLine("Emplyee is Absent");
+                    Emphrs = 0;
+                }
+
+
+
+                total_Emp_hrs = total_Emp_hrs + Emphrs;
+
+                Empwage = total_Emp_hrs * Emp_Rate_Per_Hour;
+
+
+
+                Console.WriteLine("Total Employee Wage :" + Empwage);
+            }
+        }
+    }
+
+}
